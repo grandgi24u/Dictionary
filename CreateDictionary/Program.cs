31b978a328ef9de5a5d0997df0deb907a4bfc85f
GRANDGIRARD <grandgirard.clement24@gmail.com>
2023-07-31 13:24:40 +0200

    Recreate how i read the file

--- CreateDictionary/Program.cs
+++ CreateDictionary/Program.cs
@@ -9,114 +9,75 @@ namespace CreateDictionary
 {
     public class Program
     {
-        public static void Main()
+
+        static void Main()
         {
-            // Remplacez ces chemins par les chemins vers vos fichiers réels
             string mmcifFilePath = "../../../Input/MMCIF/1A0A.mmcif";
             string ubdbAssignLogFilePath = "../../../Input/UBDBAssign/ubdbAssign_1A0A.log";
 
-            // Créez un dictionnaire pour stocker les informations d'atomes
-            Dictionary<string, Dictionary<string, string>> residueDictionary = new Dictionary<string, Dictionary<string, string>>();
-
-            // Parsez le fichier mmcif et mettez à jour le dictionnaire avec les informations d'atomes
-            ParseMmcifFile(mmcifFilePath, residueDictionary);
-
-            // Parsez le fichier .log et mettez à jour le dictionnaire avec les informations d'attribution des types d'atomes
-            ParseUbdbAssignLog(ubdbAssignLogFilePath, residueDictionary);
-
-            // Écrivez les informations dans le fichier de sortie
-            string outputFilePath = "../../../votre_fichier_sortie.txt";
-            WriteOutputToFile(outputFilePath, residueDictionary);
-        }
-
-        private static void ParseMmcifFile(string filePath, Dictionary<string, Dictionary<string, string>> residueDictionary)
-        {
-            string[] lines = File.ReadAllLines(filePath);
-
-            foreach (string line in lines)
+            /**List<string[]> res = ParseMmcifFile(mmcifFilePath);
+            foreach(string[] r in res)
             {
-                // Vérifiez si la ligne commence par "ATOM"
-                if (line.StartsWith("ATOM"))
-                {
-                    string[] columns = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine(r[0] + " " + r[1] + " " + r[2] );
+            }*/
 
-                    if (columns.Length >= 11)
-                    {
-                        string residue = columns[5];
-                        string atomName = columns[2];
-
-                        // Mettez à jour le dictionnaire avec les informations d'atome
-                        if (!residueDictionary.ContainsKey(residue))
-                            residueDictionary[residue] = new Dictionary<string, string>();
-
-                        residueDictionary[residue][atomName] = string.Empty; // Vous pouvez initialiser avec une valeur vide si nécessaire.
-                    }
-                }
-            }
+            ParseUbdbAssignLog(ubdbAssignLogFilePath);
+            string outputFilePath = "../../../votre_fichier_sortie.txt";
+            Console.ReadLine();
         }
 
-        private static void ParseUbdbAssignLog(string filePath, Dictionary<string, Dictionary<string, string>> residueDictionary)
+        static List<string[]> ParseMmcifFile(string filePath)
         {
+            List<string[]> output = new List<string[]>();
             string[] lines = File.ReadAllLines(filePath);
-
             bool startParsing = false;
             foreach (string line in lines)
             {
-                // Vérifiez si le parsing peut commencer
-                if (line.StartsWith("(2) Atom Type Assignemnt"))
+                if (line.StartsWith("_atom_site.calc_flag"))
                 {
                     startParsing = true;
                     continue;
                 }
-
-                // Vérifiez si le parsing doit s'arrêter
-                if (line.StartsWith("################################################################################"))
-                {
-                    startParsing = false;
-                    break;
-                }
-
-                // Commencez le parsing et mettez à jour le dictionnaire avec les informations d'attribution des types d'atomes
-                if (startParsing && line.Contains("   "))
+                if (startParsing)
                 {
                     string[] columns = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-                    if (columns.Length >= 3)
+                    if (columns.Length >= 9)
                     {
-                        string atomName = columns[0];
-                        string atomType = columns[2];
-
-                        foreach (var residueAtoms in residueDictionary.Values)
+                        if(columns[4] != "HOH")
                         {
-                            if (residueAtoms.ContainsKey(atomName))
-                            {
-                                residueAtoms[atomName] = atomType;
-                            }
+                            string[] toAdd = { columns[2].Replace("\"", "") + "_" + columns[0], columns[1], columns[4] };
+                            output.Add(toAdd);
                         }
                     }
                 }
             }
+            return output;
         }
 
-        private static void WriteOutputToFile(string filePath, Dictionary<string, Dictionary<string, string>> residueDictionary)
+        static List<string[]> ParseUbdbAssignLog(string filePath)
         {
-            using (StreamWriter writer = new StreamWriter(filePath))
+            List<string[]> output = new List<string[]>();
+            string[] lines = File.ReadAllLines(filePath);
+            bool startParsing = false;
+            foreach (string line in lines)
             {
-                writer.WriteLine("RESIDUE\tATOM_NAME\tATOM_TYPE");
-
-                foreach (var kvp in residueDictionary)
+                if (line.Trim().StartsWith("(2) Atom Type Assignemnt"))
                 {
-                    string residue = kvp.Key;
-
-                    foreach (var atomKvp in kvp.Value)
+                    startParsing = true;
+                    continue;
+                }
+                if (startParsing && line.Contains("   "))
+                {
+                    string[] columns = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (columns.Length >= 3)
                     {
-                        string atomName = atomKvp.Key;
-                        string atomType = atomKvp.Value;
-
-                        writer.WriteLine($"{residue}\t{atomName}\t{atomType}");
+                        string[] toAdd = { columns[0], columns[1], columns[2] };
+                        output.Add(toAdd);
                     }
                 }
             }
+            return output;
         }
+
     }
 }
